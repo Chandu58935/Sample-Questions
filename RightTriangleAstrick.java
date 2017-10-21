e7500811e516daca7277ff8d99fb1d4eb841c5d5
Chandra kiran <chandrakiran554@gmail.com>
2017-10-21 11:23:58 +0530

    Update

--- RightTriangleAstrick.java
+++ RightTriangleAstrick.java
@@ -8,6 +8,8 @@ public class RightTriangleAstrick {
     for (int i = 1;i<=rows ;i++ ) {
       for (int j=1;j<=i ;j++ ) {
         System.out.print("*");
+        //System.out.print(i);
+        //System.out.print(j);
       }
       System.out.println();
     }
